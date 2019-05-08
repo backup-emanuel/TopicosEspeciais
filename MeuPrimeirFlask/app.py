93eccafa2ac23dbfa5d20086144a1f0dba1202d4
Emanuel Gomes <emanuel.gomes@academico.ifpb.edu.br>
2019-05-08 12:06:21 -0300

    atividade flask

--- MeuPrimeirFlask/app.py
+++ MeuPrimeirFlask/app.py
@@ -6,5 +6,15 @@ app = Flask(__name__)
 def hello_world(nome):
     return ("Olá %s! Estou aprendendo Flask" %(nome), 200)
 
+@app.route("/usuario/<int:id>", methods=['GET'])
+def getUsuario(id):
+	usuarios = [{1: "JOÃO"},{2:"mARIA"},{3: "JOSÉ"}]
+	for usuario in usuarios:
+		if(id in usuario.keys()):
+			print(usuario[id])
+			return (usuario[id], 200)
+
+
 if(__name__ == '__main__'):
     app.run(host='0.0.0.0', debug=True, use_reloader=True)
+
